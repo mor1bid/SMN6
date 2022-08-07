c43db6ed12ebaace8e308cad3475b22c56c19582
De <spark49942@gmail.com>
2022-08-07 12:13:01 +0300

    std

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
 ﻿void Z1 () 
 {
+string [,] table = new string[2, 5];
+table[1, 2] = "cсло";
+for (int rows = 0; rows>2; rows++) 
+{
+    for (int col = 0; col < 5; col++) 
+    {
+        Console.Write($"{table[rows, col]}-");
+    }
+    Console.WriteLine("");
+}
 
 }
+// table[2, 3] = "воо";
+
+// }
 
 Z1();
