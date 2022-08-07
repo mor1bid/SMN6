8f74b913d69c657a15d2b691b32a8c8f25425d65
De <spark49942@gmail.com>
2022-08-07 12:27:43 +0300

    std

--- Program.cs
+++ Program.cs
@@ -19,12 +19,11 @@ for (int rows = 0; rows>2; rows++)
 void Z2 () 
 {
 string [,] table = new string[2, 5];
-table[1, 2] = "cсло";
-for (int rows = 0; rows>2; rows++) 
+for (int bar = 0; bar>2; bar++) 
 {
     for (int col = 0; col < 5; col++) 
     {
-        Console.Write($"{table[rows, col]}-");
+        Console.Write($"{table[bar, col]} ");
     }
     Console.WriteLine("");
 }
