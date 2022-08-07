4d2f5c338ce4b3c5a64fcc8dbef79fceb17fe067
De <spark49942@gmail.com>
2022-08-07 12:29:32 +0300

    std

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 {
 string [,] table = new string[2, 5];
 table[1, 2] = "cсло";
-for (int rows = 0; rows>2; rows++) 
+for (int rows = 0; rows < 2; rows++) 
 {
     for (int col = 0; col < 5; col++) 
     {
@@ -19,7 +19,7 @@ for (int rows = 0; rows>2; rows++)
 void Z2 () 
 {
 int [,] table = new int[2, 5];
-for (int bar = 0; bar>2; bar++) 
+for (int bar = 0; bar < 2; bar++) 
 {
     for (int col = 0; col < 5; col++) 
     {
