94761d6f35a1063951d5848523959a58118f11f6
De <spark49942@gmail.com>
2022-08-09 21:30:07 +0300

    Z2

--- Program.cs
+++ Program.cs
@@ -1,17 +1,39 @@
-﻿void Z1 () 
+﻿void Z () 
 {
-
+Console.WriteLine("1. ");
+string [,] table = new string[2, 5];
+table[1, 2] = "cсло";
+for (int rows = 0; rows < 2; rows++) 
+{
+    for (int col = 0; col < 5; col++) 
+    {
+        Console.Write($"{table[rows, col]} ");
+    }
+    Console.WriteLine("");
 }
-Z1();
-
-void Z2 () 
+}
+//Z();
+void Z1 () 
 {
+Console.WriteLine("Введите три числа: ");
+int a = Convert.ToInt32(Console.ReadLine()), 
+    b = Convert.ToInt32(Console.ReadLine()), 
+    c = Convert.ToInt32(Console.ReadLine());
+if (a+b<c || a+c<b || b+c<a) Console.WriteLine("Данный треугольник abc несуществует");
+else Console.WriteLine("Данный треугольник abc существует");
+}
+//Z1();
 
+void Z2() 
+{
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine()), ani = num, bni = ani, cni = num/2;
+if (ani>0) 
+{
+    ani /= 2;
+    bni -= cni;
+    cni /= 2;
+    Console.Write(bni);
+}
 }
 Z2();
-
-void Z3() 
-{
-
-}
-Z3();
