327720e717e1242999e4d495b176e7d41ba92346
De <spark49942@gmail.com>
2022-08-10 22:16:05 +0300

    DZ1f

--- Program.cs
+++ Program.cs
@@ -1,16 +1,17 @@
-﻿void Z1 () 
+﻿void DZ1 () 
 {
 Random dom = new Random();
 int ent = dom.Next(1, 11);
 int [] ray = new int [ent];
 for (int i = 0; i<ent; i++) 
 {
-    Console.WriteLine("Введите " + $"{i+1}" + "число");
+    Console.WriteLine("Введите " + $"{i+1}" + " число:");
     int num = Convert.ToInt32(Console.ReadLine());
     ray[i] = num;
 }
 int count = 0;
-Console.WriteLine("41. Из чисел ");
+Console.WriteLine();
+Console.Write("41. Из чисел ");
 for (int i = 0; i<ent; i++) 
 {
     if (ray[i] > 0) count++;
@@ -18,10 +19,11 @@ for (int i = 0; i<ent; i++)
 }
 Console.Write("\b\b - " + count + " чисел больше нуля.");
 }
-Z1();
 
-void Z2() 
+void DZ2() 
 {
 
 }
-Z2();
+
+DZ1();
+DZ2();
