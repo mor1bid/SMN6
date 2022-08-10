3f37f95673d8dcd41a1e5885f2432df8c5136ee3
De <spark49942@gmail.com>
2022-08-10 22:24:06 +0300

    DZ1f

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@
 Random dom = new Random();
 int ent = dom.Next(1, 11);
 int [] ray = new int [ent];
+Console.WriteLine("Вы можете ввести " + ent + " раз.");
 for (int i = 0; i<ent; i++) 
 {
     Console.WriteLine("Введите " + $"{i+1}" + " число:");
@@ -11,14 +12,14 @@ for (int i = 0; i<ent; i++)
 }
 int count = 0;
 Console.WriteLine();
-Console.Write("41. Из чисел ");
+Console.Write("41. Из чисел: ");
 for (int i = 0; i<ent; i++) 
 {
     if (ray[i] > 0) count++;
     Console.Write(ray[i] + ", ");
 }
-if (count>4) Console.Write("\b\b - " + count + " чисел больше нуля.");
-else Console.Write("\b\b - " + count + " числа больше нуля.");
+if (count>4) Console.Write("\b\b — " + count + " чисел больше нуля.");
+else Console.Write("\b\b — " + count + " числа больше нуля.");
 }
 
 void DZ2() 
