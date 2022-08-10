b9d0ba6ec838f02ea93c26c03ae73630bd9ec184
De <spark49942@gmail.com>
2022-08-10 22:13:25 +0300

    DZ1

--- Program.cs
+++ Program.cs
@@ -1,64 +1,27 @@
-﻿void Z () 
+﻿void Z1 () 
 {
-Console.WriteLine("1. ");
-string [,] table = new string[2, 5];
-table[1, 2] = "cсло";
-for (int rows = 0; rows < 2; rows++) 
+Random dom = new Random();
+int ent = dom.Next(1, 11);
+int [] ray = new int [ent];
+for (int i = 0; i<ent; i++) 
 {
-    for (int col = 0; col < 5; col++) 
-    {
-        Console.Write($"{table[rows, col]} ");
-    }
-    Console.WriteLine("");
+    Console.WriteLine("Введите " + $"{i+1}" + "число");
+    int num = Convert.ToInt32(Console.ReadLine());
+    ray[i] = num;
 }
-}
-//Z();
-void Z1 () 
+int count = 0;
+Console.WriteLine("41. Из чисел ");
+for (int i = 0; i<ent; i++) 
 {
-Console.WriteLine("Введите три числа: ");
-int a = Convert.ToInt32(Console.ReadLine()), 
-    b = Convert.ToInt32(Console.ReadLine()), 
-    c = Convert.ToInt32(Console.ReadLine());
-if (a+b<c || a+c<b || b+c<a) Console.WriteLine("Данный треугольник abc несуществует");
-else Console.WriteLine("Данный треугольник abc существует");
+    if (ray[i] > 0) count++;
+    Console.Write(ray[i] + ", ");
 }
-//Z1();
+Console.Write("\b\b - " + count + " чисел больше нуля.");
+}
+Z1();
 
 void Z2() 
 {
-Console.WriteLine("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine()), 
-    ani = num, 
-    sdvig = 1, 
-    cni = 0;
-while (ani>0) 
-{
-    cni += ani%2 * sdvig;
-    sdvig *= 10;
-    ani /= 2;
-    Console.Write(cni);
-}
-}
-//Z2();
 
-void Z3() 
-{
-    Random dom = new Random();
-    int si = dom.Next(2, 11); 
-    int [] ray = new int [si];
-    //int i = 0;
-    for (int i = 0; i<si; i++) 
-    {
-        int num = dom.Next(0, 11);
-        ray [i] = num;
-        Console.Write(ray[i]);
-    }
-    Console.WriteLine();
-    int [] rayc = new int [si];
-    for (int i = 0; i<si; i++) 
-    {
-        rayc [i] = ray [i];
-        Console.Write(rayc[i]);
-    }
 }
-Z3();
+Z2();
