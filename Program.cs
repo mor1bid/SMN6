6bd7f53b0fd235078dcf6442e2966dd6f1947653
De <spark49942@gmail.com>
2022-08-11 04:47:37 +0300

    DZ2-1

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 Random dom = new Random();
 int ent = dom.Next(1, 11);
 int [] ray = new int [ent];
-if (ent>4) Console.WriteLine("Вы можете ввести значение " + ent + " раз.");
+if (ent>4 && ent<=1) Console.WriteLine("Вы можете ввести значение " + ent + " раз.");
 else Console.WriteLine("Вы можете ввести значение " + ent + " раза.");
 for (int i = 0; i<ent; i++) 
 {
@@ -13,19 +13,21 @@ for (int i = 0; i<ent; i++)
 }
 int count = 0;
 Console.WriteLine();
-Console.Write("41. Из " + ent + " чисел: ");
+if (ent!=1)Console.Write("41. Из " + ent + " чисел: ");
+else Console.Write("41. Из " + ent + " числа: ");
 for (int i = 0; i<ent; i++) 
 {
     if (ray[i] > 0) count++;
     Console.Write(ray[i] + ", ");
 }
-if (count>4) Console.Write("\b\b — " + count + " чисел больше нуля.");
+if (count>4 && count == 0) Console.Write("\b\b — " + count + " чисел больше нуля.");
+else if (count==1) Console.Write("\b\b — " + count + " число больше нуля.");
 else Console.Write("\b\b — " + count + " числа больше нуля.");
 }
 
-Console.WriteLine();
 void DZ2() 
 {
+Console.WriteLine();
 Console.WriteLine("43. Введите значения двух прямых: ");
 double  b1 = Convert.ToInt32(Console.ReadLine()),
         k1 = Convert.ToInt32(Console.ReadLine()),
